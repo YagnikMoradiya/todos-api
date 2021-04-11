0066c6dbeb2367a231e506774d7e01c378332477
Yagnik Moradiya <moradiyayagnik7@gmail.com>
2021-04-11 17:24:52 +0530

    final commit

--- index.js
+++ index.js
@@ -4,20 +4,12 @@ const pool = require("./src/pool.js");
 
 const port = process.env.PORT || 3005;
 
-const devConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-};
-
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
 };
 
 pool
-  .connect(process.env.NODE_ENV === "production" ? proConfig : devConfig)
+  .connect(proConfig)
   .then(() => {
     app().listen(port, () =>
       console.log(`Listining on http://localhost:${port}`)
